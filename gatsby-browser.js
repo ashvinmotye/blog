d188bf820bb7d25ca06b0e856596127636dc7234
Ashvin Motye <ashvinmotye.web@gmail.com>
2020-04-04 21:42:06 +0400

    Enable styling for code snippets

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,8 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
+// syntax highlighting for snippets
+require("prismjs/themes/prism.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("./content/assets/css/code.css")
